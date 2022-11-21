fc1fcc7cc9448e93fcd79e02acabc81c61b93ffc
SmallPeenwow <bowdencamryn@gmail.com>
2022-11-21 20:13:34 +0200

    useLocalStorageHook Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { NewNote } from './NewNote';
+import { NewNote } from './components/NewNote';
+import { useLocalStorage } from './hooks/useLocalStorage';
 
 export type Note = {
 	id: string;
 } & NoteData;
+
+export type RawNote = {
+	id: string;
+};
+
+export type RawNoteData = {
+	title: string;
+	markdown: string;
+	tagIds: string[];
+};
 
 export type NoteData = {
 	title: string;
@@ -19,6 +30,9 @@ export type Tag = {
 };
 
 function App() {
+	const [notes, setNotes] = useLocalStorage<RawNote[]>('NOTES', []);
+	const [tags, setTags] = useLocalStorage<Tag[]>('TAGS', []);
+
 	return (
 		<Container className='my-4'>
 			<Routes>
